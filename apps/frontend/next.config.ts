6017119c95facd94f074a43c1d4f61e8d85f965f
vickvey <vickvey0303@gmail.com>
2025-07-04 22:34:40 +0530

    modified backend to set proxy and frontend designing of landing page

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:5500/api/:path*", // Proxy to backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
